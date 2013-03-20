9d0e7d2f9f36c690ab3dadafb0094d2a783b7663
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-03-19 20:32:52 -0400

    Objects Practice
    created practice codes for Objects as per the assignment

--- looney_gary_assignment3.js
+++ looney_gary_assignment3.js
@@ -1,1 +1,57 @@
-alert("JavaScript works!");
+//Gary W. Hunter Looney
+//SDI 1303: Project 3
+
+
+
+
+
+
+
+
+
+//Objects
+/*
+var arr = [true, false, " ", [], function () {}, 101];
+
+//the { is what makes this an object to jave
+var student = { 
+				id: "0000111", 
+				name: "Mike", 
+				interests: [
+							"Javascript", 
+							"HTML", 
+							"CSS", 
+							"Flash"
+				],
+				age: 503423,
+				sayHi: function () {
+									console.log("Hi");
+	}
+};
+
+var key = "id";
+
+console.log( student[ key ] );
+console.log( student.sayHi );
+console.log ( student["sayHi"] );
+
+//use if function has console.log in function
+student.sayHi ();
+student["sayHi"] ();
+student.sayHi ();
+student.sayHi = function () {
+								console.log("Get Lost!!");
+				} ;
+student.sayHi ();
+
+//for statement in objects "turns values into strings"
+for (var key in student) {
+							console.log( "key: " + key + ", value: " + student [key]);
+};
+
+//to get actual values not strings
+for (var key in student) {
+							console.log( "key: " + key + ", value: ", student [key]);
+};
+*/
+
